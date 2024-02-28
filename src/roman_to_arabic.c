9dfc754489647c829673504fec644f3a98dbc212
user39rus <115086830+user39rus@users.noreply.github.com>
2024-02-28 23:07:53 +0300

    Add files via upload

---
+++ src/roman_to_arabic.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ROM_S 7
+#define LEN_S 15
+char roman[ROM_S] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+int arabic[ROM_S] = {1, 5, 10, 50, 100, 500, 1000};
+
+void roman_notation(char *romnum);
+
+int main() {
+    char romnum[LEN_S];
+    scanf("%s", romnum);
+    roman_notation(romnum);
+    return 0;
+}
+
+void roman_notation(char *romnum) {
+    int num = 0;
+    int t = 0;
+    int len = 0;
+
+    num = 0;
+    len = strlen(romnum);
+    for (int i = len - 1; i >= 0; i--) {
+        int j;
+        for (j = 0; j < ROM_S; ++j)
+            if (roman[j] == romnum[i]) break; // 123
+
+        if (arabic[j] < t) {
+            t = arabic[j];
+            num -= t;
+        } else {
+            t = arabic[j];
+            num += t;
+        }
+    }
+    printf("%d", num);
+}
